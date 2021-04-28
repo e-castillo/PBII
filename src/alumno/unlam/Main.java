75e122f436a37a06e5b2f3150488ce3d509fce8c
Emiliano Castillo <ecastillo@fujitec.com.ar>
2021-04-28 02:12:03 -0300

    primer commit

---
+++ src/alumno/unlam/Main.java
@@ -0,0 +1,17 @@
+package alumno.unlam;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Bienvenidos a la clase de PB2");
+		System.out.println("Clase de Git");
+					
+			int j = 9;
+			j++;
+		
+			System.out.println(j);
+	
+	}
+
+}
